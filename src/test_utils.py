563ceb480f4fa9957638892dd50c791a8f64cc4c
Pommers <81735047+Pommers@users.noreply.github.com>
2026-09-13 19:13:50 -0500

    Inclusion of BCG exclusion metrics

--- src/test_utils.py
+++ src/test_utils.py
@@ -1,3 +1,8 @@
+    
+from astropy.cosmology import Planck15
+import astropy.units as u
+import numpy as np
+
 def search_cols(df, keywords=None):
 
     if keywords:
@@ -13,5 +18,29 @@ def search_cols(df, keywords=None):
     else:
         raise ValueError(f"No keywords passed to function: keywords={keywords}")
 
-    
 
+def arcmin_to_kpc(t_arcmin, z = 0.0231): 
+    # Radshift of Coma z ~ 0.231 
+    
+    # Define theta angle 
+    theta = t_arcmin * u.arcmin
+    
+    # Get angular diameter distance at redshift z (in Mpc)
+    d_A = Planck15.angular_diameter_distance(z)
+    
+    # Convert arcsec to physical kpc using the small-angle relation (d = theta * D_A)
+    d_kpc = (theta.to(u.rad).value * d_A).to(u.kpc)
+    return d_kpc
+
+def arcsec_to_kpc(t_arcsec, z = 0.0231): 
+    # Radshift of Coma z ~ 0.231 
+    
+    # Define theta angle 
+    theta = t_arcsec * u.arcsec
+    
+    # Get angular diameter distance at redshift z (in Mpc)
+    d_A = Planck15.angular_diameter_distance(z)
+    
+    # Convert arcsec to physical kpc using the small-angle relation (d = theta * D_A)
+    d_kpc = (theta.to(u.rad).value * d_A).to(u.kpc)
+    return d_kpc
